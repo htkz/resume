ab2239d2251d788261efd94b9a1088042f1617fb
htkz <htkzmo@gmail.com>
2017-10-29 15:34:23 -0400

    Update

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('#li-1').click(function(event) {
+    $('.portfolio .realBar').animate({'margin-left': 0, width: '10%'}, 700)
+});
+$('#li-2').click(function(event) {
+    $('.portfolio .realBar').animate({'margin-left': '58px', width: '47%'}, 700)
+});
+$('#li-3').click(function(event) {
+    $('.portfolio .realBar').animate({'margin-left': '222px', width: '26%'}, 700)
+});
